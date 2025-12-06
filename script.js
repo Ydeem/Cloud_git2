5cadbebf86f472b3e8921a5ed675a3bdf96790b0
Ydeem <davidkwilson271@gmail.com>
2025-12-06 19:48:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ========= CONFIG =========
-const API_BASE_URL = "https://5eeivz2isa.execute-api.us-east-1.amazonaws.com/test";
+const API_BASE_URL = "https://5eeivz2isa.execute-api.us-east-1.amazonaws.com/tests";
 // ==========================
 
 const toastContainer = document.getElementById("toastContainer");
@@ -147,4 +147,5 @@ document.getElementById("deleteEmployeeForm").addEventListener("submit", async (
     handleError("Delete Employee", err);
   }
 });
+
 
